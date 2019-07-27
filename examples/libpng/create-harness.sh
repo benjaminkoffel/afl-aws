2a206ecaabfe5decf883ea8ead303fc382eca833
Koffel <benjaminkoffel@gmail.com>
2019-07-27 22:22:33 +1000

    update cfn and example harness

--- examples/libpng/create-harness.sh
+++ examples/libpng/create-harness.sh
@@ -4,6 +4,6 @@ cd libpng
 CC=../afl-2.52b/afl-gcc ./configure --disable-shared && make -j4
 ../afl-2.52b/afl-gcc contrib/libtests/readpng.c -lm -lz .libs/libpng16.a -o harness
 mkdir input
-wget -O input/tigers.png http://www.jod.id.au/rmit/tigers.png
-tar -czvf libpng.tar.gz harness input
+wget -O input/pixel.png http://www.1x1px.me/FFFFFF-0.8.png?download=1
+tar -czvf libpng.amz2.tar.gz harness input
 # afl-fuzz -i input -o output harness
